c3b15672b0b5b5cb35561bc24b17a536e89c8be3
Trikita73 <diachenkoandrii7@gmail.com>
2023-05-04 14:32:18 +0300

    animation_2

--- js/main.js
+++ js/main.js
@@ -17,6 +17,34 @@ if(ScrollTrigger.isTouch !==1) {
             scrub: true
         }
     })
+
+    let itemsLeft = gsap.utils.toArray('.gallery__left .gallery__item')
+
+   itemsLeft.forEach(item => {
+        gsap.fromTo(item, { x: -80, opacity: 0 }, {
+            opacity: 1, x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-850',
+                end: '-100',
+                scrub: true
+            }
+        })
+   })
+
+   let itemsRight = gsap.utils.toArray('.gallery__right .gallery__item')
+
+   itemsRight.forEach(item => {
+        gsap.fromTo(item, { x: 80, opacity: 0 }, {
+            opacity: 1, x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-850',
+                end: '-100',
+                scrub: true
+            }
+        })
+   })
 }
 
 
